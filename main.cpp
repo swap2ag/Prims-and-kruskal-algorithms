0dab19f5cca8337e1ec8d7706d888a3ef2ea8a32
Sah Swapnil Agrawal <agarwal.swapnil640gmail.com>
2018-10-14 13:42:21 +0530

    made function to create graph and added corner cases for the input vertices

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #include "graph.h"
 
 using namespace std;
-void displayGraph(graph g,int n)
+int getVertex()
+{
+    int vertex;
+
+    cin>>vertex;
+    return vertex;
+}
+void displayGraph(graph g)
 {
     cout<<endl;
+    int n = g.getNumNodes();
     for (int i =0 ;i<n;i++)
     {
         for(int j=0;j<n;j++)
@@ -12,35 +20,51 @@ void displayGraph(graph g,int n)
         cout<<"\n";
     }
 }
+void createGraph(graph g)
+{
+    int n0 = g.getNumNodes();
+    int srcVertex, destVertex,cost;
 
+ while(1)
+    {
+        cout<<"Enter source vertex[0 to abort entering]: ";
+        srcVertex = getVertex();
+        if(srcVertex == 0)
+            break;
+        else if(srcVertex > n0 || srcVertex<0)
+            {
+                while(srcVertex>n0 || srcVertex<0)
+                {
+                    cout<<"Enter valid vertex [1 to "<<n0<<" ]\n";
+                    srcVertex = getVertex();
+                }
+            }
+            cout<<"Enter destination vertex[0 to abort entering]:";
+        destVertex = getVertex();
+        if(destVertex == 0)
+            break;
+        else if(destVertex > n0 || destVertex<0)
+            {
+                while(destVertex>n0 || destVertex<0)
+                {
+                    cout<<"Enter valid vertex [1 to "<<n0<<" ]\n";
+                    destVertex = getVertex();
+                }
+            }
+        cout<<"Enter the weight of edge between them: ";
+        cin>>cost;
+        g.setEdge(srcVertex-1,destVertex-1,cost);
+    }
+}
 int main()
 {
-    int n0,srcVertex,destVertex,cost;
+    int n0;
 
     cout <<"Enter number of vertices in the graph: ";
     cin>>n0;
     graph g(n0);
-
-//    graph g(n);
-    while(1)
-    {
-        cout<<"\nEnter valid vertices only [1 to "<<n0<<" ]\n";
-        cout<<"Enter source vertex[enter 0 to abort entering]: ";
-        cin>>srcVertex;
-//        if(srcVertex > n0 || srcVertex<0)
-//            cout<<"Enter valid vertex [1 to "<<n0<<" ]: ";
-        if(srcVertex == 0)
-            break;
-        cout<<"Enter destination vertex: ";
-        cin>>destVertex;
-        if(destVertex == 0)
-            break;
-        cout<<"Enter the weight of edge between them: ";
-        cin>>cost;
-        g.setEdge(srcVertex-1,destVertex-1,cost);
-
-
-    }
-    displayGraph(g,n0);
+    createGraph(g);
+//    createDefaultGraph(g);
+    displayGraph(g);
     return 0;
 }
