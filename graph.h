9f8ce1c9c05d247b07282e1bcce538817528872d
Sah Swapnil Agrawal <agarwal.swapnil640gmail.com>
2018-10-20 17:33:10 +0530

    commented graph.h fully.

--- graph.h
+++ graph.h
@@ -4,21 +4,25 @@
 class graph
 {
 
-	int *adjMat;										//Corresponding adjacency matrix which represents the graph
-	int numNodes;										//Number of nodes in the graph
-    int infCost,maxCost;
+	int *adjMat                             // adjacency matrix to represent the graph
+	int numNodes;                           // number of nodes in the graph
+    int infCost,maxCost;                    // infCost denotes the cost to be treated as infinity (ie for those vertices that have been already included in U)
+                                            // maxCost stores the maximum cost edge of the graph
 	public:
-        int *mst;
-        int mstCost;
-        graph(int n);                                            // Number of nodes have to be passed and and a graph with no edges will be constructed by this
+        int *mst;                           // Minimum Spanning Tree is stored in mst array
+        int mstCost;                        // tracks the cost of MST
+        //=====Constructor==
+        graph(int n);                       // Number of nodes have to be passed and and a graph with no edges will be constructed
+
         //=====Getters======
-        int getNumNodes();
-        int getEdge(int i, int j);          // Returns cost of edge between ith and jth node of graph otherwise returns false. This info is stored in adjacency matrix of the graph.
-        int getInfCost();
-        int getMaxCost();
+        int getNumNodes();                  // Returns number of nodes in graph
+        int getEdge(int i, int j);          // Returns cost of edge between ith and jth node of graph which is in adjacency matrix
+        int getInfCost();                   // Returns infCost variable
+        int getMaxCost();                   // Returns maxCost variable
+
         //=====Setters=====
-        void setEdge(int i,int j, int val);
-        void findMaxCost();
-        void displayMST();
+        void setEdge(int i,int j, int val); // sets val as the cost of edge between ith and jth vertices
+        void findMaxCost();                 // finds maximum cost edge in graph
+        void displayMST();                  // displays MST edges
 };
 #endif // GRAPH_INCLUDE
